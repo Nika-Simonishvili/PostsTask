8b6f7489431d672c65882229a193e4d4f350b531
NikaS <nika.simonishvili@makingscience.com>
2023-08-07 15:18:37 +0400

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<header>
+    @include('layouts.navigation')
+</header>
+<body>
+@yield('content')
+</body>
+</html>
