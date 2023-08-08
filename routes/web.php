756fa4a72c5f7352bef7546eb8ef55a51724e9fd
NikaS <nika.simonishvili@makingscience.com>
2023-08-08 14:47:55 +0400

    final touchess

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('posts.index');
 });
 
 Route::get('posts/search', [PostController::class, 'search'])->name('posts.search');
